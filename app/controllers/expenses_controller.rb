0695a648c20dcd1338e1f463751d78cd60e4ec82
ian root <ianaroot@gmail.com>
2013-12-13 00:00:52 -0800

    WIP: user can scroll through months in expenses index (currently no distinction of which year)

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,5 @@
 class ExpensesController < ApplicationController
-  MONTH_NAMES = %w(January, February, March, April, May, June, July, August, September, October, November, December)
+  MONTH_NAMES = %w(January February March April May June July August September October November December)
 
   def create
     @expense = current_user.expenses.build(params[:expense])
@@ -8,21 +8,32 @@ class ExpensesController < ApplicationController
     else
       flash[:error] =  @expense.errors.full_messages
     end
-      redirect_to root_path
+    redirect_to root_path
   end
 
   def index
     if params[:display_month_number]
-      display_month_number = params[:display_month_number]
+      @display_month_number = params[:display_month_number].to_i % 12
     else
-      display_month_number = Date.current.month
+      @display_month_number = Date.current.month
     end
     if !Expense.where(month: Date.current.month).empty?
-      @expenses = Expense.where(month: Date.current.month).order(:week)
+      @expenses = Expense.where(month: @display_month_number).order(:week)
     else
       @expenses = []
     end
-    @display_month_name = MONTH_NAMES[display_month_number - 1]
+    @display_month_names = {current: MONTH_NAMES[(@display_month_number - 1) % 12], previous: MONTH_NAMES[(@display_month_number - 2) % 12],
+                            coming: MONTH_NAMES[(@display_month_number) % 12]}
   end
 
+  private
+    def adjust_month(month_number)
+      if month_number == 0
+        month_number = 12
+      elsif  month_number == 13
+        month_number = 1
+      end
+      month_number
+    end
+  #RUDE.
 end
