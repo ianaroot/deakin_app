6d4e7a6b4a788bdc855dd304a5ac5702fe27716e
ian root <ianaroot@gmail.com>
2013-12-15 23:09:13 -0800

    change months listing on expense to go 0 to 11, thus fixing month wrapping in expenses#index

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -12,28 +12,38 @@ class ExpensesController < ApplicationController
   end
 
   def index
-    if params[:display_month_number]
-      @display_month_number = params[:display_month_number].to_i % 12
-    else
-      @display_month_number = Date.current.month
-    end
-    if !Expense.where(month: Date.current.month).empty?
-      @expenses = Expense.where(month: @display_month_number).order(:week)
-    else
-      @expenses = []
-    end
-    @display_month_names = {current: MONTH_NAMES[(@display_month_number - 1) % 12], previous: MONTH_NAMES[(@display_month_number - 2) % 12],
-                            coming: MONTH_NAMES[(@display_month_number) % 12]}
+    # @display_year = params[:display_year].to_i + ( params[:display_month_number].to_i / 12)
+    # if params[:display_month_number]
+    #   @display_month_number = params[:display_month_number].to_i % 12
+    # else
+    #   @display_month_number = Date.current.month
+    #   @display_year = Date.current.year
+    # end
+    # if !Expense.where(month: Date.current.month).empty?
+    #   @expenses = Expense.where(month: @display_month_number).order(:week)
+    # else
+    #   @expenses = []
+    # end
+    # @display_month_names = {current: MONTH_NAMES[(@display_month_number) % 12 - 1], previous: MONTH_NAMES[(@display_month_number - 1) % 12 -1],
+    #                         coming: MONTH_NAMES[(@display_month_number + 1) % 12 - 1]}
+    # puts @display_year
+    # puts @display_month_number
+    # puts "coming number is #{(@display_month_number + 1) % 13}"
+
+  if params[:display_month_number]
+    @display_month_number = params[:display_month_number].to_i
+  else
+    #see explanantion in expense model for subtracting one from month
+    @display_month_number = Date.current.month - 1
+  end
+  if !Expense.where(month: @display_month_number).empty?
+    @expenses = Expense.where(month: @display_month_number).order(:week)
+  else
+    @expenses = []
+  end
+    @display_month_names = {current: MONTH_NAMES[(@display_month_number) % 12], previous: MONTH_NAMES[(@display_month_number - 1) % 12],
+                            coming: MONTH_NAMES[(@display_month_number + 1) % 12]}
+    puts @display_month_number
   end
 
-  private
-    def adjust_month(month_number)
-      if month_number == 0
-        month_number = 12
-      elsif  month_number == 13
-        month_number = 1
-      end
-      month_number
-    end
-  #RUDE.
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -21,14 +21,17 @@ class Expense < ActiveRecord::Base
   validates :amount, presence: true
   validates :user_id, presence: true
 
+
+  def create_month
+    #month has one subtracted because it's use is in an array of months
+    #and also as we cross years, we loop with modulus,
+    #this all means that january is the 0th month and December is the 11th
+    self.month = self.date.month - 1
+  end
   private
 
   def create_week
     self.week = self.date.day / 7 + 1
-  end
-
-  def create_month
-    self.month = self.date.month
   end
   
 end
