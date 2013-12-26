a413e587531f0bb1c59bc1d6c247548f9b2265cd
ian root <ianaroot@gmail.com>
2013-12-25 20:28:04 -0800

    debts owed and owing display on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,10 @@ class StaticPagesController < ApplicationController
     if signed_in?
       @house_food = current_user.expenses.build(food: true)
       @general_expense = current_user.expenses.build(food: false)
+      @debts_owed = current_user.debts_owed.where(type: "UnpaidDebt")
+      @debts_owing = current_user.debts_owing.where(type: "UnpaidDebt")
+      puts @debts_owing
+      puts @debts_owed
     else
       redirect_to signin_path 
     end
