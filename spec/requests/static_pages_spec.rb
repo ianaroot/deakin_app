f7ac75c6ba195249e7c3229626ee360d87478cee
ian root <ianaroot@gmail.com>
2013-12-27 18:38:04 -0800

    change css id's on expense forms, all tests green

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -25,14 +25,14 @@ describe "Static pages" do
           describe "without an amout" do
             before {valid_signin(user) }
             it "it should not make a new expense" do
-              fill_in 'food_expense_date', with: "12/12/2013" 
+              fill_in 'expense_date', with: "12/12/2013" 
               expect { click_button "record food purchase" }.not_to change(Expense, :count).by(1)
             end
           end
           describe "with amount and date filled in" do
             before { valid_signin(user) }
             it "should create a new expense" do
-              fill_in 'food_expense_date', with: "12/12/2013"  
+              fill_in 'expense_date', with: "12/12/2013"  
               fill_in "food_expense_amount", with: 45
               expect { click_button "record food purchase" }.to change(Expense.where(kind: "food"), :count).by(1)
             end
@@ -44,22 +44,24 @@ describe "Static pages" do
             before { valid_signin(user) }
             it "should not make a new expense" do
               fill_in "general_expense_amount", with: 45
-              expect { click_button "record general house expense" }.not_to change(Expense, :count).by(1)
+              expect { click_button "record general purchase" }.not_to change(Expense, :count).by(1)
             end
           end
           describe "without an amout" do
             before {valid_signin(user) }
             it "it should not make a new expense" do
-              fill_in 'general_expense_date', with: "12/12/2013" 
-              expect { click_button "record general house expense" }.not_to change(Expense, :count).by(1)
+              fill_in 'expense_date', with: "12/12/2013" 
+              expect { click_button "record general purchase" }.not_to change(Expense, :count).by(1)
             end
           end
           describe "with amount and date filled in" do
             before { valid_signin(user) }
             it "should create a new expense" do
-              fill_in 'general_expense_date', with: "12/12/2013"  
+              within "#general_expense_form" do
+                fill_in 'expense_date', with: "12/12/2013"  
+              end
               fill_in "general_expense_amount", with: 45
-              expect { click_button "record general house expense" }.to change(Expense.where(kind: "general"), :count).by(1)
+              expect { click_button "record general purchase" }.to change(Expense.where(kind: "general"), :count).by(1)
             end
           end
         end
