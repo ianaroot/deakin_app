e711ffc59ea5a22ce7845c07736b492dd3a7cec8
ian root <ianaroot@gmail.com>
2013-12-30 12:38:05 -0800

    clean up expenses#controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -12,43 +12,24 @@ class ExpensesController < ApplicationController
   end
 
   def index
-    # @display_year = params[:display_year].to_i + ( params[:display_month_number].to_i / 12)
-    # if params[:display_month_number]
-    #   @display_month_number = params[:display_month_number].to_i % 12
-    # else
-    #   @display_month_number = Date.current.month
-    #   @display_year = Date.current.year
-    # end
-    # if !Expense.where(month: Date.current.month).empty?
-    #   @expenses = Expense.where(month: @display_month_number).order(:week)
-    # else
-    #   @expenses = []
-    # end
-    # @display_month_names = {current: MONTH_NAMES[(@display_month_number) % 12 - 1], previous: MONTH_NAMES[(@display_month_number - 1) % 12 -1],
-    #                         coming: MONTH_NAMES[(@display_month_number + 1) % 12 - 1]}
-    # puts @display_year
-    # puts @display_month_number
-    # puts "coming number is #{(@display_month_number + 1) % 13}"
-
-  if params[:display_month_number]
-    @display_month_number = params[:display_month_number].to_i % 12
-    @display_year = params[:display_year].to_i + (params[:display_month_number].to_i / 12)
-    puts @display_month_number
-    puts @display_year
-  else
-    #see explanantion in expense model for subtracting one from month
-    date = Date.current
-    @display_month_number = date.month - 1
-    @display_year = date.year
-  end
-  if !Expense.where(month: @display_month_number).where(year: @display_year).empty?
-    @expenses = Expense.where(month: @display_month_number).where(year: @display_year).order(:week)
-  else
-    @expenses = []
-  end
+    if params[:display_month_number]
+      @display_month_number = params[:display_month_number].to_i % 12
+      @display_year = params[:display_year].to_i + (params[:display_month_number].to_i / 12)
+      puts @display_month_number
+      puts @display_year
+    else
+      #see explanantion in expense model for subtracting one from month
+      date = Date.current
+      @display_month_number = date.month - 1
+      @display_year = date.year
+    end
+    if !Expense.where(month: @display_month_number).where(year: @display_year).empty?
+      @expenses = Expense.where(month: @display_month_number).where(year: @display_year).order(:week)
+    else
+      @expenses = []
+    end
     @display_month_names = {current: MONTH_NAMES[(@display_month_number) % 12], previous: MONTH_NAMES[(@display_month_number - 1) % 12],
                             coming: MONTH_NAMES[(@display_month_number + 1) % 12]}
-    puts @display_month_number
   end
 
 end
